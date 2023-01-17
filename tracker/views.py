ffa3e5734df6f0e5c859f98cc9a18e24e1b0e342
jayjaychukwu <liljaydon.pj@gmail.com>
2023-01-17 08:37:25 +0100

    Instructions

--- tracker/views.py
+++ tracker/views.py
@@ -11,13 +11,25 @@ class NotificationEmailAPIView(GenericAPIView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
+        """
+        Add an email that notifications will be sent to.
+        Note: Only 5 emails per account
+        """
         serializer = self.serializer_class(data=request.data)
+        # TODO: Add the 5 emails logic to the serializer or add it here
         serializer.is_valid(raise_exception=True)
         serializer.save(user=request.user)
         response = {
             "message": f"the email, {serializer.validated_data['email']}, has been added for notifications",
         }
         return Response(data=response)
+
+    def get(self, request, *args, **kwargs):
+        """
+        All the emails that notifications will be sent for an account
+        """
+        # TODO: Add the retrieval of all emails for an account here
+        pass
 
 
 class WebsitesToMonitorAPIView(GenericAPIView):
